95436e9c690eac8aaa8c22821c4c06b290ec3ad5
sahariyarahamad <djsahariyaahammed@gmail.com>
2025-06-12 15:58:50 +0600

    #

--- projects-script.js
+++ projects-script.js
@@ -1,16 +1,8 @@
 // projects html script
 const imageCoverSnpFree = document.getElementById("project-cover-id-snp-free");
-const linkSnpFree = document.querySelectorAll(".project-link");
 
 
 
 imageCoverSnpFree.src = "https://raw.githubusercontent.com/sahariyarahamad/sahariyarhost/refs/heads/main/my_project_store/assets/simple_note_project/snp_free/ic_snp_free.png";
 
-linkSnpFree.forEach(link => {
-    link.addEventListener("click", () => {
-        const name = link.getAttribute("data-name");
-        const version = link.getAttribute("data-version");
-        window.location.href = 'projectview.html?name='+name+'+&version='+version;
-    });
-});
 
